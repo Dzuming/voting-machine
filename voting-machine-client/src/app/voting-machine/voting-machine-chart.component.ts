0b846e095564471ffec154a58eb1e185236c4f64
POLISZAD <dawid.poliszak@contractors.roche.com>
2017-02-17 14:40:57 +0100

    Add d3 to application

--- voting-machine-client/src/app/voting-machine/voting-machine-chart.component.ts
+++ voting-machine-client/src/app/voting-machine/voting-machine-chart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as d3 from 'd3';
 @Component({
   selector: 'app-voting-machine-chart',
   templateUrl: './voting-machine-chart.component.html'
