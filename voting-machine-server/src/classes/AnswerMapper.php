925976082225c6283d6938fd1a081c46545431fc
Dzuming <dawidpoliszak@op.pl>
2017-02-19 09:55:48 +0100

    Code format

--- voting-machine-server/src/classes/AnswerMapper.php
+++ voting-machine-server/src/classes/AnswerMapper.php
@@ -7,21 +7,21 @@ class AnswerMapper extends Mapper
     }
     public function getAnswers() {
         $sql = "SELECT a.answer_id, q.question, a.answer_A, a.answer_B, a.answer_C, a.answer_D
-            from answers a
-            join questions q on (q.question_id = a.question_id)";
+        from answers a
+        join questions q on (q.question_id = a.question_id)";
         $stmt = $this->db->query($sql);
         $answers_from_db = $stmt->fetchAll(PDO::FETCH_ASSOC);
         $sumAnswers = $this->sumAnswers($answers_from_db);
         return json_encode($sumAnswers) ;
     }
     public function sumAnswers ($data) {
-$sum = array_reduce($data, function ($a, $b) {
-    isset($a[$b['question']]) ? $a[$b['question']]['answer_A'] += $b['answer_A'] : $a[$b['question']] = $b;  
-    isset($a[$b['question']]) ? $a[$b['question']]['answer_B'] += $b['answer_B'] : $a[$b['question']] = $b;  
-    isset($a[$b['question']]) ? $a[$b['question']]['answer_C'] += $b['answer_C'] : $a[$b['question']] = $b;  
-    isset($a[$b['question']]) ? $a[$b['question']]['answer_D'] += $b['answer_D'] : $a[$b['question']] = $b;  
-    return $a;
-});
-return $sum;
+        $sum = array_reduce($data, function ($a, $b) {
+            isset($a[$b['question']]) ? $a[$b['question']]['answer_A'] += $b['answer_A'] : $a[$b['question']] = $b;
+            isset($a[$b['question']]) ? $a[$b['question']]['answer_B'] += $b['answer_B'] : $a[$b['question']] = $b;
+            isset($a[$b['question']]) ? $a[$b['question']]['answer_C'] += $b['answer_C'] : $a[$b['question']] = $b;
+            isset($a[$b['question']]) ? $a[$b['question']]['answer_D'] += $b['answer_D'] : $a[$b['question']] = $b;
+            return $a;
+        });
+        return $sum;
     }
 }
--- voting-machine-server/src/classes/QuestionMapper.php
+++ voting-machine-server/src/classes/QuestionMapper.php
@@ -3,7 +3,7 @@ class QuestionMapper extends Mapper
 {
     public function getQuestions() {
         $sql = "SELECT q.question_id, q.question, q.answer_A, q.answer_B, q.answer_C, q.answer_D
-            from questions q";
+        from questions q";
         $stmt = $this->db->prepare($sql);
         $stmt->execute();
         $questions_from_db = $stmt->fetchAll(PDO::FETCH_ASSOC);
