05f64190144abe5844d3d5dc7f070645ffcc4fc9
POLISZAD <dawid.poliszak@contractors.roche.com>
2017-02-20 15:52:23 +0100

    Add scrollpsy to navigation

--- voting-machine-client/src/app/app.component.ts
+++ voting-machine-client/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+   ngOnInit() {
+  }
   title = 'app works!';
 }
